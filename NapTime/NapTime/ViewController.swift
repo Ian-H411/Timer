bafdf4b6081e474680a2ec4b30caa66f8967918a
Ian_H411 <ian.h411@gmail.com>
2019-08-06 11:55:06 -0600

    finished project

--- NapTime/NapTime/ViewController.swift
+++ NapTime/NapTime/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     //MARK: outlets
@@ -21,9 +22,10 @@ class ViewController: UIViewController {
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        naptimer.delegate = self
         
     }
-
+    
     @IBAction func timerButtonTapped(_ sender: UIButton) {
         if naptimer.isOn{
             naptimer.stopTimer()
@@ -50,10 +52,48 @@ class ViewController: UIViewController {
         buttonLabel.setTitleColor(buttonColor, for: .normal)
     }
     
-}
+    func presentAlert(){
+        let alertController = UIAlertController(title: "TIME'S UP", message: "Do you want to keep sleeping?", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.keyboardType = .numberPad
+            textField.placeholder = "how many more seconds of sleep"
+        }
+        let dismissAction = UIAlertAction(title: "im awake", style: .default, handler: nil)
+        
+        let snoozeAction = UIAlertAction(title: "Snooze", style: .default) { (_) in
+            if let textField = alertController.textFields?.first,
+                let inputText = textField.text, let textAsDouble = Double(inputText){
+                self.naptimer.startTimer(textAsDouble)
+                self.scheduleLocalAlert(in: textAsDouble)
+            }
+        }
+        alertController.addAction(dismissAction)
+        alertController.addAction(snoozeAction)
+        present(alertController,animated: true)
+    }
+        func scheduleLocalAlert(in timeInterval: TimeInterval) {
+            let content = UNMutableNotificationContent()
+            content.title = "times up"
+            content.body = "all of your time ran out"
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+            
+            
+            let request = UNNotificationRequest(identifier: "Naptimer", content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) { (error) in
+                if let error = error{
+                    print("notification Failed")
+                    print(error.localizedDescription)
+                    print(error)
+                }
+            }
+        }
+    }
+
 extension ViewController: NapTimerDelegate{
     func timerCompleted() {
-        
+        presentAlert()
     }
     
     func timerStopped() {
